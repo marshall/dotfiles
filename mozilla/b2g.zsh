32228bb341640b873073cd03f46aec720e046c65
Marshall Culpepper <marshall.law@gmail.com>
2012-06-14 11:06:29 -0500

    copied some good tmux/vim config from pengwynn, new tmuxinator config for B2G

--- mozilla/b2g.zsh
+++ mozilla/b2g.zsh
@@ -2,3 +2,8 @@
 if [ ! -d "/Volumes/Boot2Gecko" ]; then
     hdiutil attach ~/Documents/Boot2Gecko.dmg.sparseimage -mountpoint /Volumes/Boot2Gecko;
 fi
+
+# Some useful mozilla-central environment shortcuts
+export MOZILLA_CENTRAL=~/Code/mozilla-central
+export MC_DOM=$MOZILLA_CENTRAL/dom
+export TEST_MOBILE_NETWORKS=$MC_DOM/network/tests/marionette/test_mobile_networks.js
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias ls='ls -G'
 alias ack='nocorrect ack'
 alias adb='nocorrect adb'
 alias repo='nocorrect repo'
+alias mux='nocorrect mux'
 
 alias vi='/Applications/MacVim.app/Contents/MacOS/Vim'
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
