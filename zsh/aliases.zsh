aa2f5f6d02945e5a0a3c76e3888e44e74e9453fd
Marshall Culpepper <marshall.law@gmail.com>
2012-06-13 22:49:38 -0500

    updated vim aliases, added vim-fugitive. autoclose vim when NERDTree is the last thing open

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias ls='ls -G'
 alias ack='nocorrect ack'
 alias adb='nocorrect adb'
 alias repo='nocorrect repo'
+
+alias vi='/Applications/MacVim.app/Contents/MacOS/Vim'
+alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
