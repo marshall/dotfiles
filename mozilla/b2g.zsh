542b86dcc628d17278f0c23b8d2a010906b2730d
Marshall Culpepper <marshall.law@gmail.com>
2012-09-06 11:52:34 -0500

    lots of new mozilla/b2g specific workflow aliases

--- mozilla/b2g.zsh
+++ mozilla/b2g.zsh
@@ -12,3 +12,87 @@ export TEST_MOBILE_NETWORKS=$MC_DOM/network/tests/marionette/test_mobile_network
 
 export MARIONETTE_CLIENT=$MOZILLA_CENTRAL/testing/marionette/client/marionette
 export MARIONETTE_LOGCAT=$MARIONETTE_CLIENT/logcat
+export B2G_DIR=~/Code/B2G
+
+b2g_push_busybox() {
+  adb $@ shell 'mkdir -p /data/busybox/busybox'
+  adb $@ push $B2G_DIR/gaia/build/busybox-armv6l /data/busybox/busybox-bin
+  adb $@ shell 'chmod 755 /data/busybox/busybox-bin'
+}
+
+adb_busybox() {
+  if [[ "$1" = "-d" || "$1" = "-e" ]]; then
+    ADB_ARGS=$1
+    shift
+  elif [[ "$1" = "-s" && -n "$2" ]]; then
+    ADB_ARGS="$1 $2"
+    shift
+    shift
+  fi
+
+  adb $ADB_ARGS shell /data/busybox/busybox-bin $@
+}
+
+b2g_restart() {
+  adb $@ shell 'stop b2g; start b2g'
+}
+
+b2g_build_update_full() {
+  ./build.sh GAIA_APP_SRCDIRS=apps gecko-update-full
+}
+
+b2g_push_update_full() {
+  . load-config.sh
+  b2g_push_update $GECKO_OBJDIR/dist/b2g-update/b2g-update/b2g-gecko-update.mar
+}
+
+b2g_push_update() {
+  MAR_PATH=$1
+  if [[ ! -n "$MAR_PATH" ]]; then
+    echo "Usage: $0 [update.mar] (adb options)"
+    return 1
+  fi
+  shift
+
+  VERSION="99.0" # TODO make this changeable
+  STAGE_PATH=/tmp/b2g-updates
+
+  [[ -f "$STAGE_PATH" ]] && rm -rf $STAGE_PATH
+  mkdir -p $STAGE_PATH
+
+  cp $MAR_PATH $STAGE_PATH/b2g-gecko-update.mar
+  SHA512=$(shasum -a 512 $MAR_PATH | sed 's/ .*//')
+  BUILD_ID=$(date +%Y%m%d%H%M%S)
+  eval $(stat -s $MAR_PATH)
+
+  cat > $STAGE_PATH/update.xml <<UPDATE_TEMPLATE
+<?xml version="1.0"?>
+<updates>
+  <update type="minor" appVersion="$VERSION" version="$VERSION" extensionVersion="$VERSION" buildID="$BUILD_ID" licenseURL="http://www.mozilla.com/test/sample-eula.html" detailsURL="http://www.mozilla.com/test/sample-details.html">
+    <patch type="complete" URL="http://localhost/b2g-gecko-update.mar" hashFunction="SHA512" hashValue="$SHA512" size="$st_size"/>
+  </update>
+</updates>
+UPDATE_TEMPLATE
+
+  adb $@ push $STAGE_PATH /data/local/b2g-updates
+}
+
+b2g_start_update_httpd() {
+  adb $@ shell /data/busybox/busybox-bin httpd -h /data/local/b2g-updates
+}
+
+b2g_build() {
+  ./build.sh 2>&1 | grcat conf.gcc
+}
+
+b2g_repo_sync_manifest() {
+  if [[ ! -f ".repo/manifest.xml" ]]; then
+    echo "You must be in a B2G directory to run this" 1>&2
+    return 1
+  fi
+
+  . load-config.sh
+  rm -rf .repo/manifest* &&
+  ./repo init -u git://github.com/mozilla-b2g/b2g-manifest.git -b $DEVICE &&
+  ./repo sync
+}
--- mozilla/mozilla.zsh
+++ mozilla/mozilla.zsh
@@ -1,20 +1,78 @@
+export MOZILLA_CENTRAL=~/Code/mozilla-central
+export MOZILLA_INBOUND=~/Code/mozilla-inbound
+export MOZILLA_CENTRAL_HG=~/Code/mozilla-central-hg
 
 alias xpcom_uuidgen="uuidgen | tr '[[:upper:]]' '[[:lower:]]'"
 
 MOZ_GIT_TOOLS=~/Source/moz-git-tools
-export PATH=$PATH:$MOZ_GIT_TOOLS
+export PATH=$PATH:$MOZ_GIT_TOOLS:$DOTFILES/mozilla
+
+bz_format_patch() {
+  git format-patch -k --no-stat --no-signature --stdout $@ | git patch-to-hg-patch
+}
 
 bz_create_patch() {
   rev=HEAD^
-  if [[ ( -n $1 ) && ( ! -f $1 ) ]]; then
+  if [[ ( -n "$1" ) && ( ! -f "$1" ) ]]; then
     rev=$1
     shift
   fi
 
-  git format-patch -k --no-stat --no-signature --stdout $rev -- $@ | git patch-to-hg-patch
+  bz_format_patch $rev -- $@
 }
 
-export MOZILLA_CENTRAL=~/Code/mozilla-central
+git_commit_to_hg_patch_mc() {
+  git_commit_to_hg_patch $MOZILLA_CENTRAL_HG $@
+}
+
+git_commit_to_hg_patch_inbound() {
+  git_commit_to_hg_patch $MOZILLA_INBOUND $@
+}
+
+moz_try_git_commit() {
+  git_commit_to_hg_patch $MOZILLA_CENTRAL_HG $@
+  hg -R $MOZILLA_CENTRAL_HG trychooser
+}
+
+git_commit_to_hg_patch() {
+  if [[ "$1" = "--help" ]]; then
+    echo "Usage: git_commit_to_hg_patch (hg repo) (revisions)"
+    echo "  hg repo: path to a HG repository ($MOZILLA_CENTRAL_HG is default)"
+    echo "  revisions: revision(s) suitable for git format-patch (HEAD^..HEAD is default)"
+    return
+  fi
+
+  REPO=$MOZILLA_CENTRAL_HG
+  if [[ "$1" != "" ]]; then
+    REPO=$1
+    shift
+  fi
+
+  REVS="HEAD^"
+  if [[ "$1" != "" ]]; then
+    REVS=$@
+  fi
+
+  echo "Creating MQ patch in HG repo: $REPO"
+  echo "Using revision(s): $REVS"
+
+  hg -R $REPO pull -u
+  git_branch=$(git branch | grep '*' | sed 's/* //')
+  bz_create_patch $REVS | hg -R $REPO qimport --git --name $git_branch --push -
+}
+
+moz_git_push_inbound() {
+  if [[ ( "$1" == "-t" ) || ( "$1" == "--tip" ) ]]; then
+    ARGS=--tip $MOZILLA_INBOUND
+  else
+    ARGS=$MOZILLA_INBOUND
+  fi
+
+  # hub won't work
+  unalias git
+  /usr/local/bin/git push-to-hg $ARGS $@
+}
+
 mozbuild() {
   cd $MOZILLA_CENTRAL
   make -f client.mk build $@
