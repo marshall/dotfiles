f65fbb7b912f6c71e10fa01b706200fe05034d7b
Marshall Culpepper <marshall.law@gmail.com>
2013-07-19 15:34:08 -0500

    adding a base version of repo, and bumping logcat-color

--- android/android.zsh
+++ android/android.zsh
@@ -2,7 +2,7 @@ export ANDROID_SDK=$HOME/Applications/android-sdk-macosx
 export ANDROID_SOURCE=$HOME/Source/android
 export NDK_CCACHE=/usr/local/bin/ccache
 
-export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools
+export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$DOTFILES/android
 
 # This makes AOSP build happier in Mac OS X
 ulimit -S -n 1024
