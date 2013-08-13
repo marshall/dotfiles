3ced8c37754803c0ba69913ee7345fcf8fe9f0b1
Marshall Culpepper <marshall.law@gmail.com>
2013-08-13 10:14:21 -0500

    arduino environment vars

---
+++ arduino/arduino.zsh
@@ -0,0 +1,8 @@
+if [ "$OS_NAME" = "Darwin" ]; then
+    export ARDUINO_ROOT=/Applications/Arduino.app/Contents/Resources/Java
+fi
+
+export ARDUINO_TOOLS_AVR=$ARDUINO_ROOT/hardware/tools/avr
+export ARDUINO_TOOLS_ARM=$ARDUINO_ROOT/hardware/tools/g++_arm_none_eabi
+
+export PATH=$PATH:$ARDUINO_TOOLS_AVR/bin:$ARDUINO_TOOLS_ARM/bin
