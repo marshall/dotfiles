722bb2f0fcea9722891042e2ca2ffb3b3a9eef8b
Marshall Culpepper <marshall.law@gmail.com>
2014-04-04 12:09:35 -0500

    added hg prompt extension and tweaked zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,32 +3,31 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 git_branch() {
-  echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+    echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
 git_dirty() {
-  st=$(git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    echo -n "%{$fg_bold[green]%}[$(git_prompt_info)%{$reset_color%}"
-    echo $st | grep "nothing to commit" 1>/dev/null 2>/dev/null
-    if [[ "$?" != "0" ]]
-    then
-      echo -n "%{$fg_bold[red]%} *%{$reset_color%}"
+    st=$(git status --porcelain 2>/dev/null)
+    if [[ "$?" != "0" ]]; then
+        return
     fi
+
+    echo -n "%{$fg_bold[green]%}[$(git_prompt_info)%{$reset_color%}"
+
+    if [[ "$st" != "" ]]; then
+        echo -n "%{$fg_bold[red]%} *%{$reset_color%}"
+    fi
+
     echo "%{$fg_bold[green]%}]%{$reset_color%}"
-  fi
 }
 
 git_prompt_info() {
- ref=$(git symbolic-ref HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}"
+   ref=$(git symbolic-ref HEAD 2>/dev/null) || return
+   echo "${ref#refs/heads/}"
 }
 
 hg_prompt_info() {
-    hg prompt --angle-brackets "%{$fg_bold[green]%}[<branch>@<tags|,>]%{$reset_color%}" 2>/dev/null
+   hg prompt --angle-brackets "%{$fg_bold[green]%}[<branch>@<tags|,>]%{$reset_color%}" 2>/dev/null || return
 }
 
 venv_info() {
@@ -40,24 +39,24 @@ venv_info() {
 }
 
 current_dir() {
-  echo "%{$fg[white]%}[%~]%{$reset_color%}"
+    echo "%{$fg[white]%}[%~]%{$reset_color%}"
 }
 
 user_at_host() {
-  echo "%{$fg_bold[green]%}%n@%m%{$reset_color%}"
+    echo "%{$fg_bold[green]%}%n@%m%{$reset_color%}"
 }
 
 current_time() {
-  echo "%{$fg[blue]%}%D{[%I:%M:%S]}%{$reset_color%}"
+    echo "%{$fg[blue]%}%D{[%I:%M:%S]}%{$reset_color%}"
 }
 
 prompt_chars() {
-  echo "%{$fg[blue]%}->%{$fg_bold[blue]%} %#%{$reset_color%}"
+    echo "%{$fg[blue]%}->%{$fg_bold[blue]%} %#%{$reset_color%}"
 }
 
-export PROMPT=$'$(user_at_host) $(current_time) $(current_dir) $(git_dirty)\n$(venv_info)$(prompt_chars) '
+export PROMPT=$'$(user_at_host) $(current_time) $(current_dir) $(git_dirty)$(hg_prompt_info)\n$(venv_info)$(prompt_chars) '
 export RPROMPT='$(vi_mode_prompt_info)'
 
 precmd() {
-  title "zsh" "%m" "%55<...<%~"
+    title "zsh" "%m" "%55<...<%~"
 }
