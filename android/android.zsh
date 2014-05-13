8f98f8da2ba269236c94d8d4a8e97f1a93e01734
Marshall Culpepper <marshall.law@gmail.com>
2014-05-13 13:27:28 -0500

    changed ANDROID_SDK for linux

--- android/android.zsh
+++ android/android.zsh
@@ -1,4 +1,9 @@
-export ANDROID_SDK=$HOME/Applications/android-sdk-macosx
+if [ "$OS_NAME" = "Darwin" ]; then
+    export ANDROID_SDK=$HOME/Applications/android-sdk-macosx
+elif [ "$OS_NAME" = "Linux" ]; then
+    export ANDROID_SDK=$HOME/Apps/android-sdk-linux
+fi
+
 export ANDROID_HOME=$ANDROID_SDK
 export ANDROID_SOURCE=$HOME/Source/android
 export NDK_CCACHE=/usr/local/bin/ccache
