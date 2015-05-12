f3c4f6b1f78d5dc2a4f4948aacb8e7899311c308
Marshall Culpepper <marshall.law@gmail.com>
2015-05-12 09:57:30 -0500

    add ~/bin to $PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export PATH="$PATH:./bin"
+export PATH="$PATH:./bin:$HOME/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
