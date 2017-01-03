21ae4f801f1fc9cd158848b4c2ac51295c67b83a
Marshall Culpepper <marshall@kubos.co>
2017-01-03 10:35:58 -0600

    tweak to pass in a kubos project dir and update for new boardflash args

--- kubos/kubos.zsh
+++ kubos/kubos.zsh
@@ -1,5 +1,4 @@
-#!/bin/zsh
-
+#!/bin/zsh 
 KUBOS_DEV_DIR=$HOME/Code/kubos
 CISTACK_NODE=rpi998
 
@@ -9,19 +8,20 @@ run_cmd() {
 }
 
 kubos_cistack_flash() {
-    kubos_proj=$PWD
+    kubos_proj=${1:-$PWD}
+
     if [[ ! (-f "$kubos_proj/.yotta.json") ]]; then
-        echo "$PWD not a Kubos project"
+        echo "$kubos_proj not a Kubos project"
     fi
 
-    kubos_board=$(cat "$PWD/.yotta.json" | jq -r .build.target | sed 's/,\*//')
-    bin_name=$(cat "$PWD/module.json" | jq -r .name)
-    rel_src_dir=$(cat "$PWD/module.json" | jq -r .bin)
+    kubos_board=$(cat "$kubos_proj/.yotta.json" | jq -r .build.target | sed 's/,\*//')
+    bin_name=$(cat "$kubos_proj/module.json" | jq -r .name)
+    rel_src_dir=$(cat "$kubos_proj/module.json" | jq -r .bin)
 
-    local_path="$PWD/build/$kubos_board/$rel_src_dir/$bin_name"
+    local_path="$kubos_proj/build/$kubos_board/$rel_src_dir/$bin_name"
     remote_path="/tmp/$bin_name"
 
     run_cmd scp "$local_path" "kubos@$CISTACK_NODE:$remote_path"
     run_cmd ssh kubos@$CISTACK_NODE /bin/bash --login \
-        -c \'python /var/lib/ansible/tests/boardflash.py -f \"$bin_name\" -p /tmp -b $kubos_board\'
+        -c \'python /var/lib/ansible/tests/boardflash.py -f \"/tmp/$bin_name\" -b $kubos_board\'
 }
