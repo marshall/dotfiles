5ac2c03659ca0bd3a45f111179e9324d672b327a
Marshall Culpepper <marshall.law@gmail.com>
2020-09-23 14:06:53 -0500

    add nvm config, and only set VISUAL when VScode is installed

--- vscode/vscode.zsh
+++ vscode/vscode.zsh
@@ -1,3 +1,10 @@
 # Add Visual Studio Code (code)
-export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-export VISUAL="code --wait --reuse-window"
+
+VSCODE_APP="/Applications/Visual Studio Code.app"
+VSCODE_BIN="$VSCODE_APP/Content/Resources/app/bin"
+
+export PATH="$PATH:$VSCODE_BIN"
+
+if [[ -x "$VSCODE_BIN/code" ]]; then
+    export VISUAL="code --wait --reuse-window"
+fi
