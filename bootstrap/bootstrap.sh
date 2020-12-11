8946faa21fd55e16db5943651e31a3b1b8f2edf8
Marshall Culpepper <mcu@hey.com>
2020-12-10 18:39:48 -0600

    fixes for linux and other misc updates

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,9 +2,7 @@
 # Bootstrap a clean environment with dependencies
 set -e
 
-PIP_PACKAGES=(virtualenv virtualenvwrapper)
 RUBY_GEMS=(rake redcarpet pygments.rb)
-NPM_PACKAGES=(instant-markdown-d)
 
 THIS_DIR=$(cd `dirname "$0"`; pwd)
 OS=`uname`
@@ -26,20 +24,6 @@ case "$OS" in
 esac
 
 run_cmd git submodule update --init --recursive
-
-echo "pip requires sudo"
-run_cmd curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-run_cmd sudo python /tmp/get-pip.py
-
-echo "virtualenv and virtualenvwrapper require sudo"
-PIP_ARGS=
-if [[ "$OS" = "Darwin" ]]; then
-    PIP_ARGS="--ignore-installed six"
-fi
-run_cmd sudo pip install $PIP_ARGS ${PIP_PACKAGES[@]}
-
-echo "npm requires sudo"
-run_cmd sudo npm -g install ${NPM_PACKAGES[@]}
 
 echo "gem requires sudo"
 run_cmd sudo gem install ${RUBY_GEMS[@]}
--- bootstrap/bootstrap_linux.sh
+++ bootstrap/bootstrap_linux.sh
@@ -2,16 +2,10 @@
 # Bootstrap a clean Linux environment with dependencies
 set -e
 
-APT_PACKAGES=(zsh vim git-core grc ruby gem rake curl)
-
-install_ack() {
-    run_cmd sudo apt-get install ack-grep
-    run_cmd sudo update-alternatives --install /usr/bin/ack ack /usr/bin/ack-grep 1
-}
+APT_PACKAGES=(zsh vim git-core grc ruby gem rake curl silversearcher-ag ruby-dev build-essential)
 
 echo "apt-get requires sudo"
 run_cmd sudo apt-get install ${APT_PACKAGES[@]}
 
 echo "updating default shell to zsh"
 run_cmd sudo usermod --shell /usr/bin/zsh $USER
-install_ack
