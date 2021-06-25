f69f118f6848b02757e0b6979b59b5bfd3661409
Marshall Culpepper <mcu@hey.com>
2021-06-25 16:21:33 -0500

    avoid capturing nvm output in dotfiles-private init

--- zsh/private.zsh
+++ zsh/private.zsh
@@ -1,6 +1,5 @@
 dotfiles_private_rel="$DOTFILES/../dotfiles-private"
 
 if [[ -d "$dotfiles_private_rel" ]]; then
-    export DOTFILES_PRIVATE=$(cd "$dotfiles_private_rel"; pwd)
-    for config_file ($DOTFILES_PRIVATE/*/*.zsh) source $config_file
+    for config_file ($dotfiles_private_rel/*/*.zsh) source $config_file
 fi
