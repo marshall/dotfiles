da927765c95a3ed9fc5d021ec27e7a7f4969c05e
Marshall Culpepper <mcu@hey.com>
2021-10-08 11:15:55 -0500

    simplify gitconfig and ssh key mgmt w/ keychain

--- ssh/identity_files.awk
+++ ssh/identity_files.awk
@@ -1,11 +1,27 @@
 #!/bin/awk -f
 {
     if ($1 == "IdentityFile") {
-        split($0, path, "\"");
+        split($0, path, "\"")
         if (path[2]) {
-            print path[2];
+            files[path[2]] = ""
         } else {
-            print $2;
+            files[$2] = ""
         }
     }
+    if ($1 == "Include") {
+        # use zsh to expand the path(s)
+        cmd = "zsh -c '(for path (" $2 ") echo $path) | xargs'"
+
+        if ((cmd | getline paths) > 0) {
+            split(paths, pathsArr, " ")
+            for (i = 1; i <= length(pathsArr); i++) {
+                 parseCmd = ENVIRON["DOTFILES"] "/ssh/identity_files.awk " pathsArr[i]
+                 while ((parseCmd | getline) > 0)
+                     files[$0] = ""
+                 close(parseCmd)
+            }
+        }
+        close(cmd)
+    }
 }
+END { for ( f in files ) print f }
--- ssh/ssh.zsh
+++ ssh/ssh.zsh
@@ -1,34 +1,31 @@
-setup_ssh_agent() {
-  # Try to find an existing ssh-agent before starting a new one
-  agent_pid=$(ps -ef | grep "ssh-agent" | grep -v "grep" | awk '{print $2}')
+# in minutes
+export KEYCHAIN_TIMEOUT=60
 
-  if [[ -z $agent_pid ]]; then
-    # None exists, start ssh-agent
-    eval "$(ssh-agent)"
-  else
-    agent_ppid=$(ps -ef | grep "ssh-agent" | grep -v "grep" | awk '{print $3}')
-    agent_sock=$(find /tmp -path "*ssh*" -type s -iname "agent.$agent_ppid" 2>/dev/null)
-    echo "[ssh-agent] PID: $agent_pid, Sock: $agent_sock"
-    export SSH_AGENT_PID="$agent_pid"
-    export SSH_AUTH_SOCK="$agent_sock"
-  fi
-
-  known_keys=$(ssh-add -l | awk '{print $3}')
-  "$DOTFILES/ssh/identity_files.awk" "$HOME/.ssh/config" | sort -u | while read key; do
-    case "$key" in
-        "~/"*) key="${HOME}/${key#"~/"}";;
-    esac
-
-    if [[ -f "$key" ]]; then
-        # Only ssh-add unknown identity files
-        (echo $known_keys | grep $key > /dev/null) && continue
-
-        echo "$fg[white]Adding ssh key $reset_color$fg[blue]$key$reset_color"
-        ssh-add $key
-    fi
-  done
+ssh_identity_files() {
+    config_file=$HOME/.ssh/config
+    keys=()
+    "$DOTFILES/ssh/identity_files.awk" "$config_file" | sort -u | while read key; do
+        case "$key" in
+            "~/"*) key="${HOME}/${key#"~/"}";;
+        esac
+        keys+=("$key")
+    done
+    echo "$keys[@]"
 }
 
-if [[ "$OS_NAME" = "Linux" ]]; then
-  setup_ssh_agent
-fi
+keychain_reload() {
+    if ( which keychain >/dev/null ); then
+        keys=($(ssh_identity_files))
+        if [[ ! -f "$HOME/.keychain/$(uname -n)-sh" ]]; then
+            echo "$fg[blue][keychain]$reset_color Spawning keychain ssh-agent"
+        fi
+
+        if ( keychain --list | grep "Error connecting" ) || ( keychain --list | grep "no identities" ); then
+                echo -n "$fg[blue][keychain]$reset_color Adding $fg[green]$#keys$reset_color identity files"
+                echo ", timeout $fg[green]$KEYCHAIN_TIMEOUT${reset_color} minutes."
+        fi
+        eval $(keychain --timeout $KEYCHAIN_TIMEOUT -q --eval "$keys[@]")
+    fi
+}
+
+keychain_reload
