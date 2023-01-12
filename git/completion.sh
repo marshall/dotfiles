8f4ba31c3e3b05d9844c85103639392fa37759d7
Marshall Culpepper <mcu@hey.com>
2023-01-12 10:20:45 -0600

    graceful fallback for git completion script location

--- git/completion.sh
+++ git/completion.sh
@@ -1,1 +1,5 @@
-zstyle ':completion:*:*:git:*' script $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+if ( which brew >/dev/null ); then
+  zstyle ':completion:*:*:git:*' script $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+elif [[ -f /usr/share/bash-completion/completions/git ]]; then
+  zstyle ':completion:*:*:git:*' script /usr/share/bash-completion/completions/git
+fi
