7b3c4f5ad470616ae25b8493327f05340a85e2dc
Marshall Culpepper <mcu@hey.com>
2023-01-12 10:26:55 -0600

    only set NODE_PATH in homebrew envs

--- nodejs/nodejs.zsh
+++ nodejs/nodejs.zsh
@@ -1,3 +1,5 @@
-# NODE_PATH for npm / node installed via brew
-DEFAULT_NODE_PATH=$(brew --prefix)/lib/node_modules
-export NODE_PATH=$(/usr/bin/env npm root -g 2>/dev/null || echo "$DEFAULT_NODE_PATH")
+if ( which brew >/dev/null ); then
+  # NODE_PATH for npm / node installed via brew
+  DEFAULT_NODE_PATH=$(nvm which current | sed "s/bin\/node/lib\/node_modules/")
+  export NODE_PATH=$(/usr/bin/env npm root -g 2>/dev/null || echo "$DEFAULT_NODE_PATH")
+fi
