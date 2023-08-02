bd2a8f65e1dc4c317a40715f3fe5864cf0b5249c
Marshall Culpepper <mcu@hey.com>
2023-08-02 14:04:57 -0500

    move nvm config to nodejs to preserve order

--- nodejs/nodejs.zsh
+++ nodejs/nodejs.zsh
@@ -1,3 +1,31 @@
+# load the first found installation of nvm
+for dir in "$HOMEBREW_PREFIX/opt/nvm" "$HOME/.nvm"; do
+    [ -s "$dir/nvm.sh" ] && . "$dir/nvm.sh"  # This loads nvm
+    [ -s "$dir/etc/bash_completion.d/nvm" ] && . "$dir/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+    [ -s "$dir/nvm.sh" ] && break
+done
+
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
+
 if ( which brew >/dev/null ); then
   # NODE_PATH for npm / node installed via brew
   DEFAULT_NODE_PATH=$(nvm which current | sed "s/bin\/node/lib\/node_modules/")
