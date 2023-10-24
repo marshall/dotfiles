a1e63c108e0781232150f71d84be0df5c54f8f5e
Marshall Culpepper <mcu@hey.com>
2023-10-23 22:55:25 -0500

    add lsp container for clangd

--- neovim/nvim.config/lua/plugins/lang.lua
+++ neovim/nvim.config/lua/plugins/lang.lua
@@ -8,4 +8,46 @@ return {
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
+  {
+    "NoahTheDuke/vim-just",
+  },
+  {
+    "lspcontainers/lspcontainers.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers.clangd.cmd = require("lspcontainers").command("clangd", {
+        image = "coruscant-dev:1.17.0",
+        workdir = vim.fn.getenv("HOME") .. "/Code",
+        cmd_builder = function(runtime, workdir, image, network, _)
+          local volume = "--volume=" .. workdir .. ":" .. workdir .. ":z"
+          return {
+            runtime,
+            "container",
+            "run",
+            "--interactive",
+            "--rm",
+            "--network=" .. network,
+            "--workdir=" .. workdir,
+            volume,
+            image,
+            "clangd",
+            "--background-index",
+          }
+        end,
+      })
+      return opts
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      ensure_installed = {
+        "cue",
+      },
+    },
+  },
 }
