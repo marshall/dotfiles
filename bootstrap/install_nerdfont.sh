f29fae725f51f6d74ebf2a3c04fe5f680aec3e9e
Marshall Culpepper <mcu@hey.com>
2024-07-17 10:30:32 -0500

    bump to nerd fonts v3.2.1

--- bootstrap/install_nerdfont.sh
+++ bootstrap/install_nerdfont.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 FONT_FAMILY="Meslo"
-FONT_NAME="$FONT_FAMILY LG M DZ Regular Nerd Font Complete"
-FONT_PATH="$FONT_FAMILY/M-DZ/Regular/complete/$FONT_NAME.ttf"
+FONT_VARIANT="LGMDZNerdFont"
+NERD_FONTS_VERSION=3.2.1
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 
-curl -fL "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$FONT_FAMILY.zip" -o $FONT_FAMILY.zip
-unzip $FONT_FAMILY.zip "$FONT_NAME.ttf"
+curl -fL "https://github.com/ryanoasis/nerd-fonts/releases/download/v$NERD_FONTS_VERSION/$FONT_FAMILY.zip" -o $FONT_FAMILY.zip
+unzip $FONT_FAMILY.zip \
+    "$FONT_FAMILY$FONT_VARIANT-Regular.ttf" \
+    "$FONT_FAMILY$FONT_VARIANT-Bold.ttf" \
+    "$FONT_FAMILY$FONT_VARIANT-Italic.ttf" \
+    "$FONT_FAMILY$FONT_VARIANT-BoldItalic.ttf"
+
 rm $FONT_FAMILY.zip
 
 fc-cache -f -v
